46e0305eb37f01f636a3933f0a695dd1b4499f03
ya_boiko <acc.v.boiko@gmail.com>
2023-08-03 23:38:09 +0300

    updated user in module services

--- services/User.py
+++ services/User.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-@dataclass(slots=True)
+@dataclass(frozen=True, slots=True)
 class User:
     id: int | None
     tg_id: int
--- services/UserService.py
+++ services/UserService.py
@@ -33,28 +33,26 @@ class UserService:
         if not result:
             return None
 
-        user = UserService.create(
+        return User(
+            id=result.get("id"),
             tg_id=result.get("tg_id"),
             login=result.get("login"),
             name=result.get("name"),
             surname=result.get("surname"),
         )
-        user.id = result.get("id")
-        return user
 
     def get_by_id(self, user_id: int) -> User | None:
         result = self.queries.get_user_by_id(user_id)
         if not result:
             return None
 
-        user = UserService.create(
+        return User(
+            id=result.get("id"),
             tg_id=result.get("tg_id"),
             login=result.get("login"),
             name=result.get("name"),
             surname=result.get("surname"),
         )
-        user.id = result.get("id")
-        return user
 
     def is_exists_in_db(self, tg_id: int) -> bool:
         return bool(self.queries.get_user_by_tg_id(tg_id))
