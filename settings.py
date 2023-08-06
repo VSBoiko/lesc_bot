abcde123833c065ac3a4a24fa1684cfa366f3dae
ya_boiko <acc.v.boiko@gmail.com>
2023-08-07 00:11:20 +0300

    updated settings - added keys DB_USER and DB_USER_PASS

--- settings.py
+++ settings.py
@@ -8,7 +8,8 @@ load_dotenv(dotenv_path=dotenv_path)
 
 TOKEN = os.getenv("TOKEN")
 DB_NAME = os.getenv("DB_NAME")
-DB_PATH = os.path.join(os.path.dirname(__file__), DB_NAME)
+DB_USER = os.getenv("DB_USER")
+DB_USER_PASS = os.getenv("DB_USER_PASS")
 
-db_datetime_format = "%d.%m.%Y %H:%M:%S"
+db_datetime_format = "%d.%m.%Y %H:%M:%S"            # Для базы SQLite - поле с датой имеет тип TEXT
 str_datetime_format = "%d %B %H:%M"
