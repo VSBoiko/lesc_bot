365bf8ec5d69226796861ea12000e9397d9e4d56
ya_boiko <acc.v.boiko@gmail.com>
2023-09-30 16:48:43 +0300

    WIP

--- TgButtons.py
+++ TgButtons.py
@@ -1,32 +1,17 @@
-from datetime import datetime
+from typing import Any
 
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from api.meetings.Meeting import Meeting
-from api.settings import datetime_format_str
-from clb_queries import ClbShowDetail, ClbPostfix
 
-
-def get_meetings_btn_builder(meetings: list[Meeting]) -> InlineKeyboardBuilder:
-    if not meetings:
-        InlineKeyboardBuilder()
-
-    def get_date_time(m: Meeting) -> datetime:
-        return m.get_date_time()
-
-    meetings.sort(key=get_date_time)
-    btn_builder = InlineKeyboardBuilder()
-    for meeting in meetings:
-        meeting_date: str = meeting.get_date_time().strftime(datetime_format_str)
-        place_name: str = meeting.get_place().get_name()
-        btn_builder.button(
-            text=f"{meeting_date} - {place_name}",
-            callback_data=ClbShowDetail(
-                postfix=ClbPostfix.meeting,
-                pk=meeting.get_pk()
-            ),
+class TgButtons:
+    @staticmethod
+    def add(builder: InlineKeyboardBuilder, text: str, callback_data: Any) -> InlineKeyboardBuilder:
+        builder.button(
+            text=text,
+            callback_data=callback_data,
         )
+        return builder
 
-    btn_builder.adjust(1)
-
-    return btn_builder
+    @staticmethod
+    def get_empty_builder() -> InlineKeyboardBuilder:
+        return InlineKeyboardBuilder()
